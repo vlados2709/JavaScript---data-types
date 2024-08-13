5ee087b53f729d83b51197fd09d4d94051e4df22
Vlad <suvorkin.phonk@gmail.com>
2024-08-13 12:37:16 +0300

    Complete commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Перша умова
+let money = parseFloat(prompt("Введіть суму грошей:"));
+let chocolatePrice = parseFloat(prompt("Введіть ціну однієї шоколадки:"));
 
+let chocolatesBought = Math.floor(money / chocolatePrice);
+let change = money % chocolatePrice;
+
+alert(`Ви можете купити ${chocolatesBought} шоколадок. Ваша здача: ${change.toFixed(2)} грн.`);
+
+// Друга умова
+let number = parseInt(prompt("Введіть тризначне число:"));
+
+let reversedNumber = (number % 10) * 100 + Math.floor((number % 100) / 10) * 10 + Math.floor(number / 100);
+
+alert(`Число задом наперед: ${reversedNumber}`);
